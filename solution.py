ec831bc583a972b48e7d896dab9a1fa72656f905
Kooreis <kooreis.official@gmail.com>
2024-01-28 07:14:46 -0600

    Defining the HitCounter class with methods to record a hit and to count the number of hits in the last five minutes.

--- solution.py
+++ solution.py
@@ -1,2 +1,11 @@
-import time
-from collections import deque
+class HitCounter:
+    def __init__(self):
+        self.hits = deque()
+
+    def record_hit(self):
+        self.hits.append(time.time())
+
+    def hits_in_last_five_minutes(self):
+        while self.hits and time.time() - self.hits[0] > 300:
+            self.hits.popleft()
+        return len(self.hits)
